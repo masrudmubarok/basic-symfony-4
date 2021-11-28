fcdd13f387f91e321792079b24e7b7014f641843
masrudmubarok <masrud.mubarok21@gmail.com>
2021-11-28 23:36:46 +0700

    Advancedd routes

--- course/19. Advanced Routes.php
+++ course/19. Advanced Routes.php
@@ -1,57 +1,37 @@
 <?php
-
+// src/Controller/DefaultController.php
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
 use App\Entity\User;
 use App\Services\GiftsService;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
-
-    # GiftService using construction
-    // public function __construct(GiftsService $gifts)
-    // {
-    //     $gifts->gifts = ['a','b','c'];
-    // }
 
     /**
      * @Route("/", name="default")
      */
     public function index(GiftsService $gifts)
     {
-        # Users array
-        // $users = ['Masrud','Mubarok'];
-
-        # Insert users to database
-        // $entityManager = $this->getDoctrine()->getManager();
-        // $user = new User;
-        // $user->setName('Ahmad');
-        // $user2 = new User;
-        // $user2->setName('Mubarok');
-        // $entityManager->persist($user);
-        // $entityManager->persist($user2);
-        // exit($entityManager->flush());
-
-        # Read users form Sqlite database
+        // $users = [];
         $users = $this->getDoctrine()->getRepository(User::class)->findAll();
-
+        
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
             'users' => $users,
             'random_gift' => $gifts->gifts,
         ]);
-        
     }
-
+    
     /**
-     * @Route("/blog/{page?}", name="blog list", requirements={"page"="\d+"})
+     * @Route("/blog/{page?}", name="blog_list", requirements={"page"="\d+"})
      */
     public function index2()
     {
-        return new Response('Optional parameters in url and requirement for parameters, the parameters should be digit or number');
+        return new Response('Optional parameters in url and requirements for parameters');
     }
 
     /**
@@ -73,11 +53,13 @@ class DefaultController extends AbstractController
     /**
      * @Route({
      *      "nl": "/over-ons",
-     *      "en": "/about-us",
+     *       "en": "/about-us"
      * }, name="about_us")
      */
     public function index4()
     {
         return new Response('Translated routes');
     }
+    
+    
 }
