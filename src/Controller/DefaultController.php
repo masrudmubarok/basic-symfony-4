437cbdc828904b669b2c5f21885ba32b45724979
masrudmubarok <masrud.mubarok21@gmail.com>
2021-12-04 15:23:07 +0700

    More features in controllers

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 class DefaultController extends AbstractController
 {
@@ -22,10 +23,10 @@ class DefaultController extends AbstractController
     // }
 
     # Binding Services (logger) to Controllers
-    public function __construct($logger)
-    {
-        // use service logger
-    }
+    // public function __construct($logger)
+    // {
+    //     // use service logger
+    // }
 
     /**
      * @Route("/", name="default")
@@ -44,9 +45,6 @@ class DefaultController extends AbstractController
         // $entityManager->persist($user);
         // $entityManager->persist($user2);
         // exit($entityManager->flush());
-
-        # Read users form Sqlite database
-        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
 
         # Flash Messages
         // $this->addFlash(
@@ -102,6 +100,9 @@ class DefaultController extends AbstractController
         //     throw $this->createNotFoundException('The users do not exist');
         // }
 
+        # Read users form Sqlite database
+        $users = $this->getDoctrine()->getRepository(User::class)->findAll();
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
             'users' => $users,
@@ -143,5 +144,62 @@ class DefaultController extends AbstractController
     public function index4()
     {
         return new Response('Translated routes');
+    }
+
+    /**
+     * @Route("/generate-url{param?}", name="generate_url")
+     */
+    public function generate_url()
+    {
+        exit($this->generateUrl(
+            'generate_url',
+            array('param' => 10),
+            UrlGeneratorInterface::ABSOLUTE_URL
+        ));
+    }
+
+    /**
+     * @Route("/download")
+     */
+    public function download()
+    {
+        $path = $this->getParameter('download_directory');
+        return $this->file($path.'file.pdf');
+    }
+
+    /**
+     * @Route("/redirect-test")
+     */
+    public function redirectTest()
+    {
+        return $this->redirectToRoute('route_to_redirect', array('param' => 10));
+    }
+
+    /**
+     * @Route("/url-to-redirect/{param?}", name="route_to_redirect")
+     */
+    public function methodToRedirect()
+    {
+        exit('Test redirection');
+    }
+
+    /**
+     * @Route("/forwarding-to-controller")
+     */
+    public function forwardingToController()
+    {
+        $response = $this->forward(
+            'App\Controller\DefaultController::methodToForwardTo',
+            array('param' => '21')
+        );
+        return $response;
+    }
+
+    /**
+     * @Route("/url-to-forward-to/{param?}", name="route_to_forward_to")
+     */
+    public function methodToForwardTo($param)
+    {
+        exit('Test controller forwarding - '.$param);
     }
 }
